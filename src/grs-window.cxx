6938c7ceef1d8ad4c6861d34da2bf6228c0a5f1c
Jason Spitzak <jason.spitzak@gmail.com>
2022-11-01 21:28:07 -0400

    made a basic GL window open succesfully

--- src/grs-window.cxx
+++ src/grs-window.cxx
@@ -1,15 +1,46 @@
 #include "grs.h"
 
 
-grsWindow::grsWindow(int xscale, int yscale, ScreenType screenType){
-        this->windowFL = new Fl_Window(xscale, yscale);
-        if (screenType == ScreenType::grs_GLScreen){
-            this->windowFL->end();
-            this->windowFL->show();
-        }
-        
-    };
+class grs_GL_Window : public Fl_Gl_Window {
+    void draw();
+    public:
+    grs_GL_Window(int X, int Y, int W, int H, const char *L = 0)
+    : Fl_Gl_Window(X, Y, W, H, L) {};
+};
 
-    int grsWindow::draw() {
-        return Fl::run();
-    };
+void grs_GL_Window::draw() {
+        if (!valid()) {
+            valid(1);
+            glLoadIdentity();
+            glViewport(0,0,pixel_w(),pixel_h());
+        }
+        glClearColor(0.0,0.0,0.0,1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glEnd();
+        printf("inside draw function \n");
+}
+
+
+
+
+
+grsWindow::grsWindow(int xscale, int yscale, ScreenType screenType){
+    this->windowFL = new Fl_Window(xscale, yscale);
+    if (screenType == ScreenType::grs_GLScreen){
+
+        
+
+        this->windowFL->add( new grs_GL_Window(0,0,xscale,yscale) );
+
+
+    }
+        //grs_GL_Window MyWindow(0,0,xscale,yscale);
+        this->windowFL->end();
+        this->windowFL->show();
+        // MyWindow.show();
+        
+};
+
+// int grsWindow::draw() {
+//     //return Fl::run();
+// };
--- src/main.cxx
+++ src/main.cxx
@@ -2,10 +2,12 @@
 
 
 
-grsWindow myWindow = grsWindow(1000,500,ScreenType::grs_GLScreen);
+grsWindow myWindow = grsWindow(500,500,ScreenType::grs_GLScreen);
 
 
 int main(){
-  return myWindow.draw();
+  //return myWindow.draw();
+    return Fl::run();
 
 }
+
