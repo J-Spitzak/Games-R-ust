df3e8e80728087046a7eb5411f66324206a358fc
Jason Spitzak <jason.spitzak@gmail.com>
2023-04-20 18:10:23 -0400

    made stuff work

--- src/grs-GL-drawing.h
+++
@@ -1,5 +0,0 @@
-#include "include.h"
-
-void grs_line(int x1, int y1, int x2, int y2, grsColors color);
-
-void grs_rectangle(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, grsColors color);
--- src/grs-objects.cxx
+++
@@ -1,1 +0,0 @@
-#include "grs-objects.h"
